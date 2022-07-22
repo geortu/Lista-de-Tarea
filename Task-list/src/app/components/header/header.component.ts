7caeebbf0b49b260e09c6107097216e9b8f08d8c
geortu <jorge.arturo.biazzo@gmail.com>
2022-07-22 12:18:00 -0300

    header components

--- Task-list/src/app/components/header/header.component.ts
+++ Task-list/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title:string = 'Mi Lista de Tareas';
   constructor() { }
 
   ngOnInit(): void {
