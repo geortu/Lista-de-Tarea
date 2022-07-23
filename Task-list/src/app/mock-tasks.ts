d95a82cce1fb4a89acc2523bc6cb1f1344e6e207
geortu <jorge.arturo.biazzo@gmail.com>
2022-07-23 19:31:48 -0300

    Lista de tareas

---
+++ Task-list/src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import{Task} from "./Tasks"
+export const TASKS :Task[]=[
+    {
+        id:1,
+        text:"Terminar la tarea 1",
+        day: '25 de julio',
+        reminder:true
+    },
+    {
+        id:2,
+        text:"Terminar la tarea 2",
+        day: '01 de agosto',
+        reminder:true
+    },
+    {
+        id:3,
+        text:"Terminar la tarea 3",
+        day: '07 de agosto',
+        reminder:true
+    }, 
+    {
+        id:4,
+        text:"Terminar la tarea 4",
+        day: '14 de agosto',
+        reminder:true
+    }
+]
